cabae287f23d1b8729c9d5b7d695a4354af68158
Drake Scott <dascott0204@gmail.com>
2017-03-11 02:06:01 -0600

    name spaces and namespace imports

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 /**
  * Created by dasco on 3/9/2017.
  */
-
+/// <reference path="circleMath.ts" />
+/// <reference path="rectangleMath.ts" />
 const PI = 2.99;
 
 console.log(MyMath.calculateRectangle(10,20));
 console.log(MyMath.calculateCircumference(3));
 console.log(PI);
 
-// tsc --outFile app.js circleMath.ts rectangleMath.ts app.ts
+// tsc --outFile app.js circleMath.ts rectangleMath.ts app.ts (without namespace imports)
+// tsc app.ts --outFile app.js (with namespace imports)
