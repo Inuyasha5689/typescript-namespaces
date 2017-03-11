48da328f0cbc2cb5cf1e1106d7813522626b6e09
Drake Scott <dascott0204@gmail.com>
2017-03-11 02:08:54 -0600

    bit of extra namespace stuff

--- app.ts
+++ app.ts
@@ -3,10 +3,13 @@
  */
 /// <reference path="circleMath.ts" />
 /// <reference path="rectangleMath.ts" />
+
+import CircleMath = MyMath.Circle;
+
 const PI = 2.99;
 
 console.log(MyMath.calculateRectangle(10,20));
-console.log(MyMath.calculateCircumference(3));
+console.log(CircleMath.calculateCircumference(3));
 console.log(PI);
 
 // tsc --outFile app.js circleMath.ts rectangleMath.ts app.ts (without namespace imports)
--- circleMath.ts
+++ circleMath.ts
@@ -2,10 +2,12 @@
  * Created by dasco on 3/11/2017.
  */
 namespace MyMath {
-    const PI = 3.14;
+    export namespace  Circle{
+        const PI = 3.14;
 
-    export function calculateCircumference(diameter: number): number {
-        return diameter * PI;
+        export function calculateCircumference(diameter: number): number {
+            return diameter * PI;
+        }
     }
 
 }
